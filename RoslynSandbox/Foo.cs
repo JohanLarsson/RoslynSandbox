eb883b54f19206271dd45df11d54cb1787d29190
Johan Larsson <johan_gavle1@hotmail.com>
2017-09-17 23:27:39 +0200

    Use paket.

--- RoslynSandbox/Foo.cs
+++ RoslynSandbox/Foo.cs
@@ -1,36 +1,9 @@
-﻿using System.ComponentModel;
-using System.Runtime.CompilerServices;
-
-namespace RoslynSandbox
+﻿namespace RoslynSandbox
 {
-    public class Foo : INotifyPropertyChanged
+    using System.IO;
+
+    public class Foo
     {
-        private int _value;
-
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        public int Value
-        {
-            get
-            {
-                return _value;
-            }
-
-            set
-            {
-                if (value == _value)
-                {
-                    return;
-                }
-
-                _value = value;
-                OnPropertyChanged();
-            }
-        }
-
-        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
+        private readonly Stream stream = File.OpenRead(string.Empty);
     }
 }
