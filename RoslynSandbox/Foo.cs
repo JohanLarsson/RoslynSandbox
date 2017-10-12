df31ab09033ca73a13fa26a3499b086ce72ba9cc
Johan Larsson <johan_gavle1@hotmail.com>
2017-10-13 01:42:53 +0200

    Refactor dependencies.

--- RoslynSandbox/Foo.cs
+++ RoslynSandbox/Foo.cs
@@ -1,6 +1,21 @@
 ﻿namespace RoslynSandbox
 {
-    public class ViewModel : Caliburn.PresentationFramework.PropertyChangedBase
+    public class ViewModel : GalaSoft.MvvmLight.ObservableObject
     {
+        private int name;
+
+        public string Greeting => $"Hello{this.Name}";
+
+        public int Name
+        {
+            get { return this.name; }
+            set
+            {
+                if (this.Set(ref this.name, value))
+                {
+                    this.RaisePropertyChanged(() => this.Greeting);
+                }
+            }
+        }
     }
 }
