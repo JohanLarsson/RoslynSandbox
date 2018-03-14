3680f2f67c98836b8fafc5f502480e1c2e91adf3
JohanLarsson <johan_gavle1@hotmail.com>
2018-03-14 15:36:12 +0100

    Update dependencies.

---
+++ RoslynSandbox/Foo.cs
@@ -0,0 +1,15 @@
+﻿namespace RoslynSandbox
+{
+    using System;
+    using NUnit.Framework;
+
+    public class Foo
+    {
+        [Test]
+        public void Test()
+        {
+            //new Mock<IFormatProvider>()
+            //mock1.SetupGet(x => x.IsSatisfied).Returns(true)
+        }
+    }
+}
